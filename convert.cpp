cb4e356c65cbec8d1d5fd0eae6638ad311754c02
stormbay <ranger.su@gmail.com>
2014-05-08 22:51:33 +0800

    change test bmp file

--- convert.cpp
+++ convert.cpp
@@ -37,13 +37,23 @@ int main( int argc, char *argv[] )
 	int ret = 0;
 	CGeneralTools tools;
 
-	tools.DataConvert( "RGB32.bmp", "RGB32.rgb", 320, 240, BMP, RGB32, 1, 0, 80 );
-	tools.DataConvert( "RGB32.rgb", "RGB24.rgb", 320, 240, RGB32, RGB24, 1, 0, 80 );
-	tools.DataConvert( "RGB24.rgb", "RGB24-1.jpg", 320, 240, RGB24, JPEG444, 1, 0, 80 );
-	tools.DataConvert( "RGB24.rgb", "RGB24.yuv", 320, 240, RGB24, YUV444, 1, 0, 80 );
-	tools.DataConvert( "RGB24.yuv", "RGB24-blk.yuv", 320, 240, YUV444, YUV444BLK, 1, 0, 80 );
-	tools.DataConvert( "RGB24-blk.yuv", "RGB24-2.jpg", 320, 240, YUV444BLK, JPEG444, 1, 0, 80 );
+//	tools.DataConvert( "RGB32.bmp", "RGB32.rgb", 320, 240, BMP, RGB32, 1, 0, 80 );
+//	tools.DataConvert( "RGB32.rgb", "RGB24.rgb", 320, 240, RGB32, RGB24, 1, 0, 80 );
+//	tools.DataConvert( "RGB24.rgb", "RGB24-1.jpg", 320, 240, RGB24, JPEG444, 1, 0, 80 );
+//	tools.DataConvert( "RGB24.rgb", "RGB24.yuv", 320, 240, RGB24, YUV444, 1, 0, 80 );
+//	tools.DataConvert( "RGB24.yuv", "RGB24-blk.yuv", 320, 240, YUV444, YUV444BLK, 1, 0, 80 );
+//	tools.DataConvert( "RGB24-blk.yuv", "RGB24-yuv444.yuv", 320, 240, YUV444BLK, YUV444, 1, 0, 80 );
+//	tools.DataConvert( "RGB24-yuv444.yuv", "RGB24-yuv444blk.yuv", 320, 240, YUV444, YUV444BLK, 1, 0, 80 );
+//	tools.DataConvert( "RGB24-yuv444blk.yuv", "RGB24-2.jpg", 320, 240, YUV444BLK, JPEG444, 1, 0, 80 );
 
+	tools.DataConvert( "RGB32.bmp", "RGB32.rgb", 128, 32, BMP, RGB32, 1, 0, 80 );
+	tools.DataConvert( "RGB32.rgb", "RGB24.rgb", 128, 32, RGB32, RGB24, 1, 0, 80 );
+	tools.DataConvert( "RGB24.rgb", "RGB24-1.jpg", 128, 32, RGB24, JPEG444, 1, 0, 80 );
+	tools.DataConvert( "RGB24.rgb", "RGB24.yuv", 128, 32, RGB24, YUV444, 1, 0, 80 );
+	tools.DataConvert( "RGB24.yuv", "RGB24-blk.yuv", 128, 32, YUV444, YUV444BLK, 1, 0, 80 );
+	tools.DataConvert( "RGB24-blk.yuv", "RGB24-yuv444.yuv", 128, 32, YUV444BLK, YUV444, 1, 0, 80 );
+	tools.DataConvert( "RGB24-yuv444.yuv", "RGB24-yuv444blk.yuv", 128, 32, YUV444, YUV444BLK, 1, 0, 80 );
+	tools.DataConvert( "RGB24-yuv444blk.yuv", "RGB24-2.jpg", 128, 32, YUV444BLK, JPEG444, 1, 0, 80 );
 
 	return ret;
 }
